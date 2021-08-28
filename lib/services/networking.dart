5a29e4e750f8e7d71d6c031636a917b80b624409
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-08-28 20:50:15 +0000

    Used API key

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const apiKey="2fb1d1f24e95db0c7193031c10589f72";
+
 class Network {
   void getData() async {
     http.Response response = await http.get(
       Uri.parse(
-          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'),
+          'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=$apiKey'),
     );
     if (response.statusCode == 200) {
       String data = response.body;
